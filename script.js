ff678d7a9188dd4eb8f20c943eb0af80d4497938
YoanTsanev <joan.al2001@gmail.com>
2023-05-06 22:38:38 +0300

    Created Scroll to Top Button
    Created Scroll to Top Button with scroll progress. The button was added to every page, and tested successfully.

--- script.js
+++ script.js
@@ -13,3 +13,8 @@ if (close){
         nav.classList.remove('active');
     })
 }
+
+/*This one here prevents the scroll up effect when pressing X */
+document.getElementById("close").addEventListener("click", function(event){
+    event.preventDefault();
+});
